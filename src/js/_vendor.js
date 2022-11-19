aade03054e8c743503f32b9e9e5af355ea68f0b2
Ruslan <ruslan.mirets@gmail.com>
2022-11-19 16:38:28 +0300

    Chat

--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,2 +1,4 @@
 // import "../../node_modules/jquery/dist/jquery.min.js";
 // import "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import { Fancybox } from "@fancyapps/ui/src/Fancybox/Fancybox.js";
+import "@fancyapps/ui/src/Fancybox/Fancybox.js";
